48961414024ede42dcd05f05628f322b40974d53
g4rb4g3 <g4rb4g3@gmail.com>
2025-05-20 14:17:56 +0200

    add gestures

--- packages/react-native-carplay/src/interfaces/GestureEvent.ts
+++ packages/react-native-carplay/src/interfaces/GestureEvent.ts
@@ -1,41 +1,30 @@
+/**
+ * @param translation screen coordinate offset
+ * @param velocity only reported on iOS/CarPlay
+ */
 export interface PanGestureWithTranslationEvent {
   translation: { x: number; y: number };
-  /**
-   * only reported on iOS/CarPlay
-   */
   velocity?: { x: number; y: number };
 }
 
 /**
+ * @param x screen coordinate of the pinch center
+ * @param y screen coordinate of the pinch center
+ * @param scaleFactor value > 0.0 and < 2.0 when 1.0 means no scaling, > 1.0 zoom in and < 1.0 zoom out, value == 2.0 is a double tap
  * @namespace Android
  */
 export interface PinchGestureEvent {
-  /**
-   * screen coordinate of the pinch center
-   */
   x: number;
-  /**
-   * screen coordinate of the pinch center
-   */
   y: number;
-  /**
-   * scale factor
-   * value > 0.0 and < 2.0 when 1.0 means no scaling, > 1.0 zoom in and < 1.0 zoom out
-   * value == 2.0 is a double tap
-   */
   scaleFactor: number;
 }
 
 /**
+ * @param x screen coordinate of the press event
+ * @param y screen coordinate of the press event
  * @namespace Android
  */
 export interface PressEvent {
-  /**
-   * screen coordinate of the press event
-   */
   x: number;
-  /**
-   * screen coordinate of the press event
-   */
   y: number;
 }
