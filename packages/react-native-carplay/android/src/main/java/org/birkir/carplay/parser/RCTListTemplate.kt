1c93b1f9b7d847865fd6348ae96d82ff9c1a52e0
g4rb4g3 <g4rb4g3@gmail.com>
2025-05-28 10:39:40 +0200

    list template improvements

--- packages/react-native-carplay/android/src/main/java/org/birkir/carplay/parser/RCTListTemplate.kt
+++ packages/react-native-carplay/android/src/main/java/org/birkir/carplay/parser/RCTListTemplate.kt
@@ -33,7 +33,28 @@ class RCTListTemplate(
       setLoading(props.isLoading())
 
       // Sections
-      props.getArray("sections")?.let {
+      val sections = props.getArray("sections")
+      val items = props.getArray("items")
+
+      if (sections != null && items != null) {
+        throw IllegalArgumentException("invalid template configuration, use either sections or items, not both!")
+      }
+
+      // in case we get a section list that has only on section and no title we treat it as single list similar to items
+      val singleListItems =
+        if (sections?.size() == 1 && !sections.getMap(0).hasKey("header")) sections.getMap(0)
+          .getArray("items") else items
+
+      // Single List
+      singleListItems?.let {
+        setSingleList(
+          parseItemList(it)
+        )
+        return@apply
+      }
+
+      // Sections
+      sections?.let {
         for (i in 0 until it.size()) {
           val section = it.getMap(i)
           val header = section.getString("header")
@@ -46,13 +67,7 @@ class RCTListTemplate(
         }
       }
 
-      // Single List
-      // @todo handle when sections and items are defined at once.
-      props.getArray("items")?.let {
-        setSingleList(
-          parseItemList(it)
-        )
-      }
+
 
     }.build()
   }
