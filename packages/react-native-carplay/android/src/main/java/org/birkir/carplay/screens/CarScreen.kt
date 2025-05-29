d538efa4d72752c27e8c458c7638afac623af302
g4rb4g3 <g4rb4g3@gmail.com>
2025-05-29 13:07:29 +0200

    add splash screen for cluster displays

--- packages/react-native-carplay/android/src/main/java/org/birkir/carplay/screens/CarScreen.kt
+++ packages/react-native-carplay/android/src/main/java/org/birkir/carplay/screens/CarScreen.kt
@@ -1,5 +1,6 @@
 package org.birkir.carplay.screens
 
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
@@ -12,6 +13,7 @@ import androidx.car.app.navigation.model.NavigationTemplate
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import org.birkir.carplay.utils.AppInfo
 import org.birkir.carplay.utils.EventEmitter
 import org.birkir.carplay.utils.VirtualRenderer
 
@@ -77,10 +79,9 @@ class CarScreen(
       }.build()
     }
 
-    val appNameId = carContext.resources.getIdentifier("app_name", "string", carContext.packageName)
-    val appNme = if (appNameId != 0) carContext.resources.getString(appNameId) else "RNCarPlay"
+    val appName = AppInfo.getApplicationLabel(carContext)
 
-    return MessageTemplate.Builder(appNme).apply {
+    return MessageTemplate.Builder(appName).apply {
       setIcon(CarIcon.APP_ICON)
     }.build()
   }
